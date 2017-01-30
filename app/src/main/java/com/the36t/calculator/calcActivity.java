2e4428726c8a11b41043657c3a363dcabef25d03
harisek36 <harisek36@gmail.com>
2017-01-30 14:27:55 -0600

    implemented complete feature

--- app/src/main/java/com/the36t/calculator/calcActivity.java
+++ app/src/main/java/com/the36t/calculator/calcActivity.java
@@ -1,5 +1,6 @@
 package com.the36t.calculator;
 
+import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,19 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class calcActivity extends AppCompatActivity {
+
+
+    String runningNumber = "";
+    String leftValue = "";
+    String rightValue = "";
+    Operations currentOPeration;
+    int result = 0;
+
+    public enum Operations {
+        ADD, SUBTRACT, MULTIPLY, DIVIDE, EQUAL
+    }
+
+    TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,145 +46,192 @@ public class calcActivity extends AppCompatActivity {
         Button divButton = (Button) findViewById(R.id.divideButton);
         Button equalButton = (Button) findViewById(R.id.equalButton);
 
-        TextView resultView = (TextView)findViewById(R.id.answer);
+        resultView = (TextView) findViewById(R.id.answer);
+
+
+        resultView.setText("");
 
         //setting up onclick listner
 
-        oneButton.setOnClickListener(new View.OnClickListener(){
+        oneButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v){
+                                         @Override
+                                         public void onClick(View v) {
+                                             numberPressed(1);
 
-            }
-        }
+                                         }
+                                     }
         );
-        twoButton.setOnClickListener(new View.OnClickListener(){
+        twoButton.setOnClickListener(new View.OnClickListener() {
 
-                                          @Override
-                                          public void onClick(View v){
-
-                                          }
-                                      }
+                                         @Override
+                                         public void onClick(View v) {
+                                             numberPressed(2);
+                                         }
+                                     }
         );
-        threeButton.setOnClickListener(new View.OnClickListener(){
+        threeButton.setOnClickListener(new View.OnClickListener() {
 
                                            @Override
-                                           public void onClick(View v){
-
+                                           public void onClick(View v) {
+                                               numberPressed(3);
                                            }
                                        }
         );
-        fourButton.setOnClickListener(new View.OnClickListener(){
+        fourButton.setOnClickListener(new View.OnClickListener() {
 
                                           @Override
-                                          public void onClick(View v){
-
+                                          public void onClick(View v) {
+                                              numberPressed(4);
                                           }
                                       }
         );
-        fiveButton.setOnClickListener(new View.OnClickListener(){
+        fiveButton.setOnClickListener(new View.OnClickListener() {
 
                                           @Override
-                                          public void onClick(View v){
-
+                                          public void onClick(View v) {
+                                              numberPressed(5);
                                           }
                                       }
         );
-        sixButton.setOnClickListener(new View.OnClickListener(){
+        sixButton.setOnClickListener(new View.OnClickListener() {
 
                                          @Override
-                                         public void onClick(View v){
-
+                                         public void onClick(View v) {
+                                             numberPressed(6);
                                          }
                                      }
         );
-        sevenButton.setOnClickListener(new View.OnClickListener(){
+        sevenButton.setOnClickListener(new View.OnClickListener() {
 
                                            @Override
-                                           public void onClick(View v){
-
+                                           public void onClick(View v) {
+                                               numberPressed(7);
                                            }
                                        }
         );
-        eightButton.setOnClickListener(new View.OnClickListener(){
+        eightButton.setOnClickListener(new View.OnClickListener() {
 
                                            @Override
-                                           public void onClick(View v){
-
+                                           public void onClick(View v) {
+                                               numberPressed(8);
                                            }
                                        }
         );
-        nineButton.setOnClickListener(new View.OnClickListener(){
+        nineButton.setOnClickListener(new View.OnClickListener() {
 
                                           @Override
-                                          public void onClick(View v){
-
+                                          public void onClick(View v) {
+                                              numberPressed(9);
                                           }
                                       }
         );
-        zeroButton.setOnClickListener(new View.OnClickListener(){
+        zeroButton.setOnClickListener(new View.OnClickListener() {
 
                                           @Override
-                                          public void onClick(View v){
-
+                                          public void onClick(View v) {
+                                              numberPressed(0);
                                           }
                                       }
         );
-        addButton.setOnClickListener(new View.OnClickListener(){
+        addButton.setOnClickListener(new View.OnClickListener() {
 
                                          @Override
-                                         public void onClick(View v){
+                                         public void onClick(View v) {
+                        processOperation(Operations.ADD);
+                                         }
+                                     }
+        );
+        subButton.setOnClickListener(new View.OnClickListener() {
+
+                                         @Override
+                                         public void onClick(View v) {
+processOperation(Operations.SUBTRACT);
+                                         }
+                                     }
+        );
+        mulButton.setOnClickListener(new View.OnClickListener() {
+
+                                         @Override
+                                         public void onClick(View v) {
+processOperation(Operations.MULTIPLY);
+                                         }
+                                     }
+        );
+        divButton.setOnClickListener(new View.OnClickListener() {
+
+                                         @Override
+                                         public void onClick(View v) {
+processOperation(Operations.DIVIDE);
+                                         }
+                                     }
+        );
+        dotButton.setOnClickListener(new View.OnClickListener() {
+
+                                         @Override
+                                         public void onClick(View v) {
 
                                          }
                                      }
         );
-        subButton.setOnClickListener(new View.OnClickListener(){
-
-                                         @Override
-                                         public void onClick(View v){
-
-                                         }
-                                     }
-        );
-        mulButton.setOnClickListener(new View.OnClickListener(){
-
-                                         @Override
-                                         public void onClick(View v){
-
-                                         }
-                                     }
-        );
-        divButton.setOnClickListener(new View.OnClickListener(){
-
-                                         @Override
-                                         public void onClick(View v){
-
-                                         }
-                                     }
-        );
-        dotButton.setOnClickListener(new View.OnClickListener(){
-
-                                         @Override
-                                         public void onClick(View v){
-
-                                         }
-                                     }
-        );
-        clearButton.setOnClickListener(new View.OnClickListener(){
+        clearButton.setOnClickListener(new View.OnClickListener() {
 
                                            @Override
-                                           public void onClick(View v){
-
+                                           public void onClick(View v) {
+                                               leftValue = "";
+                                               rightValue= "";
+                                                result = 0;
+                                               runningNumber = "0";
+                                               currentOPeration = null;
+                                               resultView.setText(runningNumber);
                                            }
                                        }
         );
-        equalButton.setOnClickListener(new View.OnClickListener(){
+        equalButton.setOnClickListener(new View.OnClickListener() {
 
                                            @Override
-                                           public void onClick(View v){
-
+                                           public void onClick(View v) {
+                    processOperation(Operations.EQUAL);
                                            }
                                        }
         );
+
     }
-}
+        void numberPressed(int number){
+        runningNumber += String.valueOf(number);
+        resultView.setText(runningNumber);
+        }
+
+        void processOperation(Operations operations){
+
+            if(currentOPeration != null){
+                if(runningNumber != ""){
+                    rightValue = runningNumber;
+                    runningNumber = "";
+                    switch(currentOPeration){
+                        case ADD:
+                            result = Integer.parseInt(leftValue) + Integer.parseInt(rightValue);
+                            break;
+                        case SUBTRACT:
+                            result = Integer.parseInt(leftValue) - Integer.parseInt(rightValue);
+                            break;
+                        case MULTIPLY:
+                            result = Integer.parseInt(leftValue) * Integer.parseInt(rightValue);
+                            break;
+                        case DIVIDE:
+                            result = Integer.parseInt(leftValue) / Integer.parseInt(rightValue);
+                            break;
+                    }
+
+                    leftValue = String.valueOf(result);
+                    resultView.setText(leftValue);
+                }
+            }else{
+                leftValue = runningNumber;
+                runningNumber = "";
+            }
+            currentOPeration = operations;
+        }
+    }
+
+
